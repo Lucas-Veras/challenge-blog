c5bad8270fd081f40f2916fdb4285fb9c6911d46
Lucas-Veras <verasluca00@gmail.com>
2024-11-10 15:30:47 -0300

    fix: user migration

--- backend/users/migrations/0004_populate_users.py
+++ backend/users/migrations/0004_populate_users.py
@@ -39,26 +39,21 @@ def populate(apps, schema_editor):
     model_user = apps.get_model("users", "User")
     model_group = apps.get_model("auth", "Group")
 
-    try:
-        for user_data in users:
-            person_instance = model_person.objects.create(
-                name=user_data["name"],
-            )
-            common_user, created = model_group.objects.get_or_create(
-                name=GroupUserEnum.COMMON_USER,
-            )
-            person_instance.save()
-            user_instance = model_user.objects.create(
-                email=user_data["email"],
-                is_superuser=user_data["is_superuser"],
-                is_staff=user_data["is_staff"],
-                person=person_instance,
-                password=make_password(user_data["password"]),
-            )
-            user_instance.groups.add(common_user)
-            user_instance.save()
-    except Exception as e:
-        print(f"Erro ao criar usuário {user_data['email']}: {e}")
+    for user_data in users:
+        person_instance, created = model_person.objects.get_or_create(
+            name=user_data["name"],
+        )
+        common_user, created = model_group.objects.get_or_create(
+            name=GroupUserEnum.COMMON_USER,
+        )
+        user_instance, created = model_user.objects.get_or_create(
+            email=user_data["email"],
+            is_superuser=user_data["is_superuser"],
+            is_staff=user_data["is_staff"],
+            person=person_instance,
+            password=make_password(user_data["password"]),
+        )
+        user_instance.groups.add(common_user)
 
 
 class Migration(migrations.Migration):
